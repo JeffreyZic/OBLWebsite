09687e79663cf168c37cf2b3ac8bb6fbdf5a2a7e
Jeffrey <zic.jeffrey@gmail.com>
2017-03-26 15:00:39 -0400

    test: teste la connexion du db
    On veux voir si dbconnect() asserte qu'il est connecté ou non

--- test/test.js
+++ test/test.js
@@ -25,5 +25,41 @@ describe('homepage', function() {
                 expect($('.news').html()).to.equal('League News');
             });
         });
+
+       /* describe('getNews', function() {
+            //testing function that grabs news items from db
+
+            it('generates the proper select query when fetching news', function() {
+                var date = '20170101',
+                    subject = 'news title',
+                    author = 'john doe',
+                    text = 'this is a news article';
+                var results = [{ id: 0, date: date, subject: subject, author: author, text: text }]
+                var fields = ['id', 'date', 'subject', 'author', 'text'];
+                var expectation = mysqlMock.expects('query')
+                    .withArgs('select * from news where date', [ date ])
+                    .callsArgsWith(2, null, results, fields);
+
+                return newsItems.getNewsByDate(date).should.eventually.become(results[0]);
+            });
+
+        });*/
     });
 });
+
+describe('database', function() {
+
+    var url = "http://localhost:5000";
+
+    it('says when connection is successful', function() {
+        expect(dbconnect().to.equal('The database connection was successful'));
+    });
+
+    it ('says when connection fails', function() {
+        expect(dbconnect().to.equal('Failed to sucessfully connect to dataabse'));
+    });
+
+    it('checks if table exists', function() {
+
+    });
+}
